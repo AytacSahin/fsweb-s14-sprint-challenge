a9fc0a24c65d1c3063e67c6b563b934adb1d3fb6
AytacSahin <haytacsahin@gmail.com>
2023-06-23 16:25:23 +0300

    package installations & gitignore & server setup

--- api/server.js
+++ api/server.js
@@ -1,1 +1,19 @@
 // serverı buraya yazın ve index.js require yazın
+
+const express = require("express");
+
+const ProjectRouter = require("./project/router");
+const ResourceRouter = require("./resource/router");
+const TaskRouter = require("./task/router");
+
+const server = express();
+
+server.get("/", (req, res, next) => {
+    res.send("<h1>Challenge Projesine Hoşgeldiniz...</h1></br><h2>Bu projede, bu hafta öğrendiğin bilgilerin tamamını kullanman gerekecek!!</h2>")
+})
+
+// server.use("/api/project", ProjectRouter);
+// server.use("/api/resource", ResourceRouter);
+// server.use("/api/task", TaskRouter);
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Server'ı buradan başlatın
+
+const server = require("./api/server.js");
+
+const PORT = process.env.PORT || 9000;
+
+server.listen(PORT, (req, res, next) => {
+    console.log(`Server listening on ${PORT}...`)
+})
+
